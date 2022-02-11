24566dca65f92d316628613e34c834a1e54db85a
NanaDeu <nd.nat.nd@gmail.com>
2022-02-12 02:34:59 +0300

    Commit 1

--- src/MyException.java
+++ src/MyException.java
@@ -3,10 +3,7 @@ public class MyException {
     String regexArab = "^([1-9]|10)\\s[+*/-]\\s([1-9]|10)$";
     String regexRoman = "^(I|II|III|IV|V|VI|VII|VIII|IX|X)\\s[+*/-]\\s(I|II|III|IV|V|VI|VII|VIII|IX|X)$";
     void checkingExceptions() throws Exception {
-        if (str.matches(regexArab)) {
-         //   System.out.println("Всё ОК, это арабские");
-        } else if (str.matches(regexRoman)) {
-         //   System.out.println("Всё ОК, это римские");
+        if ((str.matches(regexArab)) || (str.matches(regexRoman))) {
         } else {
             System.err.println("Некорректный ввод");
             throw new Exception();
